0798c81d342f26df9d95b6487cc33e95a43356d3
dammy <dammy@example.com>
2023-03-05 16:11:03 +0900

    fix: mecab

--- src/audio.py
+++ src/audio.py
@@ -21,10 +21,10 @@ class Audio():
         self.isMeaningSrv = rospy.Service("/isMeaning", isMeaning, self.calbkIsMeaning)
 
     def calbkIsMeaning(self, msg):
-        return is_meaning(msg.text, msg.word_list, msg.path)
+        return is_meaning(msg.text, msg.word_list)
 
     def calbkSTT(self, msg):
-        return recognize_speech(msg.print_partial, msg.use_break, msg.return_extract_person_name, msg.remove_space, msg.voskLogLevel, msg.path)
+        return recognize_speech(msg.print_partial, msg.use_break, msg.return_extract_person_name, msg.remove_space, msg.voskLogLevel)
     
     def callback(self, msg):
         textToSpeech(msg.data, verbose=True)
