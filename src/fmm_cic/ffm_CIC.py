95d17e4a29bddb3a721b5303b43de7bee244cd8d
hiro-oh <ohshima.hiroyuki2911@gmail.com>
2023-04-21 23:20:56 +0900

    cicのimportを追加

--- src/fmm_cic/ffm_CIC.py
+++ src/fmm_cic/ffm_CIC.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from speech_and_NLP.src.tools.speech_to_text.speechToText import recognize_speech #音声認識
+from speech_and_NLP.src.tools.speech_to_text.isMeaning import is_meaning #文章の中に単語を検索する
+from speech_and_NLP.src.tools.text_to_speech.textToSpeech import textToSpeech #発話
+from speech_and_NLP.src.tools.speech_to_text.extractPersonName import extractPersonName #人名取得
 class CIC():
     def __init__(self):
+        
 
     def main(self):
         
