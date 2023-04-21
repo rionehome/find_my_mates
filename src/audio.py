cb9ef5d618e14a132741471e8257892515c66f1d
dammy <dammy@example.com>
2023-04-21 12:40:23 +0900

    write-control_system

--- src/audio.py
+++ src/audio.py
@@ -10,9 +10,9 @@ import os
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 # 音声認識の関数 (vosk)
-from speech_and_NLP.src.tools.speech_to_text.speechToText import recognize_speech 
+from speech_and_NLP.src.speechToText import recognize_speech 
 from speech_and_NLP.src.tools.speech_to_text.isMeaning import is_meaning
-from speech_and_NLP.src.tools.text_to_speech.textToSpeech import textToSpeech
+from speech_and_NLP.src.textToSpeech import textToSpeech
 
 class Audio():
     def __init__(self):
